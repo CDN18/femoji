e260f580c0589964d39626b3f2af7bc1c378dd9d
CDN18 <cardinal@codeword.info>
2024-10-05 17:10:15 +0800

    fix: tweak download
    - log: rename emoji to shortcode
    - add another rate-limit check before redirect
    - continue on file io error

--- internal/download/download.go
+++ internal/download/download.go
@@ -47,25 +47,38 @@ func Download(authClient *auth.Client, instance string, override bool) error {
 		if emoji.Category == "" {
 			emoji.Category = "uncategorized"
 		}
-		slog.Info("downloading emoji", "emoji", emoji.Shortcode, "category", emoji.Category, "url", emoji.URL)
+		slog.Info("downloading emoji", "shortcode", emoji.Shortcode, "category", emoji.Category, "url", emoji.URL)
 		dir := fmt.Sprintf("%s/%s", instance, emoji.Category)
 		// create dir if not exists
 		if _, err := os.Stat(dir); os.IsNotExist(err) {
 			if err := os.MkdirAll(dir, 0o755); err != nil {
-				return err
+				slog.Error("failed to create directory", "error", err, "shortcode", emoji.Shortcode, "path", dir)
+				continue
 			}
 		}
 		// download emoji
 		extension := filepath.Ext(emoji.URL)
 		filePath := fmt.Sprintf("%s/%s%s", dir, emoji.Shortcode, extension)
 		if _, err := os.Stat(filePath); err == nil && !override {
-			slog.Info("skipping download as it already exists", "emoji", emoji.Shortcode, "path", filePath)
+			slog.Info("skipping download as it already exists", "shortcode", emoji.Shortcode, "path", filePath)
 			continue
 		}
 		resp, err := http.Get(emoji.URL)
 		if err != nil {
-			slog.Error("failed to download emoji", "error", err, "emoji", emoji.Shortcode, "url", emoji.URL)
+			slog.Error("failed to download emoji", "error", err, "shortcode", emoji.Shortcode, "url", emoji.URL)
 			continue
+		}
+		// check x-ratelimit-remaining and x-ratelimit-reset headers
+		if resp.Header.Get("x-ratelimit-remaining") == "0" {
+			slog.Error("rate limit exceeded")
+			resetTime, err := time.Parse(time.RFC1123, resp.Header.Get("x-ratelimit-reset"))
+			if err != nil {
+				slog.Error("failed to parse x-ratelimit-reset header, using default of 300 seconds", "error", err)
+				// default to 300 seconds
+				resetTime = time.Now().Add(300 * time.Second)
+			}
+			slog.Info("rate limit will reset at", "time", resetTime)
+			time.Sleep(time.Until(resetTime))
 		}
 		// follow 3xx redirects
 		if resp.StatusCode >= 300 && resp.StatusCode < 400 {
@@ -78,20 +91,20 @@ func Download(authClient *auth.Client, instance string, override bool) error {
 		}
 		defer resp.Body.Close()
 		if resp.StatusCode != http.StatusOK {
-			slog.Error("failed to download emoji", "status", resp.StatusCode, "emoji", emoji.Shortcode, "url", emoji.URL)
+			slog.Error("failed to download emoji", "status", resp.StatusCode, "shortcode", emoji.Shortcode, "url", emoji.URL)
 			continue
 		}
 		file, err := os.Create(filePath)
 		if err != nil {
-			slog.Error("failed to create file", "error", err, "emoji", emoji.Shortcode, "path", filePath)
-			return err
+			slog.Error("failed to create file", "error", err, "shortcode", emoji.Shortcode, "path", filePath)
+			continue
 		}
 		defer file.Close()
 		if _, err := io.Copy(file, resp.Body); err != nil {
-			slog.Error("failed to write to file", "error", err, "emoji", emoji.Shortcode, "path", filePath)
-			return err
+			slog.Error("failed to write to file", "error", err, "shortcode", emoji.Shortcode, "path", filePath)
+			continue
 		}
-		// check x-ratelimit-remaining and x-ratelimit-reset headers
+		// check rate-limit, again
 		if resp.Header.Get("x-ratelimit-remaining") == "0" {
 			slog.Error("rate limit exceeded")
 			resetTime, err := time.Parse(time.RFC1123, resp.Header.Get("x-ratelimit-reset"))
